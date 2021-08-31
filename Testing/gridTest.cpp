9674b7b07f9b498991d8e48660fa443285a078a2
Ugne B <ugne.balciunaite.19@ucl.ac.uk>
2021-08-31 15:44:19 +0300

    Updated PartA grid class tests.

--- Testing/gridTest.cpp
+++ Testing/gridTest.cpp
@@ -3,22 +3,23 @@
 #include "../Code/Lib/grid.hpp"
 #include <iostream>
 #include <vector>
-/*
+
 // Part A
 // Q2
 TEST_CASE( "Check Grid is instantiated" ) 
 {
   Grid g = Grid(5, 5);
-  g.printGrid();
+  // g.printGrid();
   REQUIRE(g.getStatus(0,0) == '-');
 }
 
 // Q3
 TEST_CASE( "Check alive cells are set randomly" ) 
 {
-  Grid g = Grid(5, 5, 20);
-  g.printGrid();
+  std::cout << "Printing grid of randomly placed live cells \n"; 
+  Grid g = Grid(5, 5, 5);
 
+// Confirm the number of alive cells 
   int alive_cells = 0; 
   for (int i = 0; i < 5; i++)
   {
@@ -30,27 +31,37 @@ TEST_CASE( "Check alive cells are set randomly" )
         }
       }
   }
-  REQUIRE(alive_cells == 20);
+  REQUIRE(alive_cells == 5);
 }
-*/
+
+TEST_CASE( "Confirm different instances generate different patterns" ) 
+{
+  std::cout << "Printing different instances to check for different patterns \n";
+  Grid g = Grid(5, 5, 5);
+  Grid e = Grid(5, 5, 5);
+  Grid f = Grid(5, 5, 5);
+  // REQUIRE();
+}
 
 // Q4
-TEST_CASE("Read and print text files")
+/*
+TEST_CASE( "Check class can read and print text files" )
 {
   Grid g = Grid("../Testing/Data/glider.txt");
-  // Grid g1 = Grid("../Testing/Data/oscillators.txt");
- // Grid g2 = Grid("../Testing/Data/still_lifes.txt");
+  Grid g1 = Grid("../Testing/Data/oscillators.txt");
+  Grid g2 = Grid("../Testing/Data/still_lifes.txt");
   
   // REQUIRE();
 } 
+*/
 
 // Q5
-
-TEST_CASE("Count Live Neighbours of a cell")
+TEST_CASE( "Check Live Neighbours of a cell are counted" ) 
 {
+  std::cout << "Printing from glider.txt file \n"; 
   Grid g1 = Grid("../Testing/Data/glider.txt");
   
-  REQUIRE(g1.liveNeighbours(1, 2) == 1);
+  // REQUIRE(g1.liveNeighbours(1, 2) == 1);
   REQUIRE(g1.liveNeighbours(2, 2) == 3);
 } 
 
